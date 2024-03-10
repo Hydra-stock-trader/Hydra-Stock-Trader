9eb5dc7f101ba7720f5d7c148c09d950bdc265fb
blue-codes-yep <linzblue5@gmail.com>
2024-03-09 22:23:33 -0600

    Removed to dict function, as we are returning the dict from the stock_data function now.

--- src/endpoints/endpoints.py
+++ src/endpoints/endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from typing import Any, Dict
 from src.models.models import  Item
 from src.logic.dummy import get_item
 from src.logic.stock_data import get_stock_data
@@ -10,10 +11,10 @@ async def read_item(item_id: int):
     item = get_item(item_id)
     return item
 
-@router.get("/stock/{ticker_symbol}")
+@router.get("/stock/{ticker_symbol}", response_model=Dict[str, Any])
 async def stock(ticker_symbol: str):
     try:
         data = get_stock_data(ticker_symbol)
-        return {"ticker_symbol": ticker_symbol, "data": data.to_dict()}
+        return {"ticker_symbol": ticker_symbol, "data": data}
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
