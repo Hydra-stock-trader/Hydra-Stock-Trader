a65291eab9536f3fc3374c29a74e97ee6a9cb033
Carson Szeder <129720994+Carsoncantcode@users.noreply.github.com>
2024-03-10 17:31:58 -0700

    SEC Branch add tickers endpoints (Needs leading 0s ) (#7)
    Co-authored-by: Blue <linzblue5@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.endpoints.endpoints import router
+from src.endpoints.sec_endpoints import router as sec_router
 
 app = FastAPI()
 
@@ -15,6 +16,7 @@ app.add_middleware(
 
 # Include the router
 app.include_router(router)
+app.include_router(sec_router)
 
 @app.get("/")
 async def root():
--- src/logic/dummy.py
+++
@@ -1,6 +0,0 @@
-from src.models.models import  Item
-
-
-def get_item(item_id: int):
-    item = Item(id=item_id, name="Sample Item", description="This is a sample item", price=9.99)
-    return item
