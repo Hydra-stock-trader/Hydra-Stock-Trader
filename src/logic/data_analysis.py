ea744a0134d596886c5818165237a0ac0da554aa
blue-codes-yep <linzblue5@gmail.com>
2024-03-16 07:29:55 -0500

    Small refactor just changing feature days, and removing some whitespace

--- src/logic/data_analysis.py
+++ src/logic/data_analysis.py
@@ -3,7 +3,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, r
 from sklearn.model_selection import train_test_split
 from .baseline_model import prepare_data, train_baseline_model
 
-def evaluate_model(stock_data, feature_days=5, threshold_percentage=0.01):
+def evaluate_model(stock_data, feature_days=200, threshold_percentage=0.01):
     """
     Evaluates the baseline model on the given stock data and returns MSE, MAE, and a strict form of "accuracy"
     based on the closing price predictions being within a narrow threshold percentage of the actual values.
@@ -26,12 +26,10 @@ def evaluate_model(stock_data, feature_days=5, threshold_percentage=0.01):
     model = train_baseline_model(X_train, y_train)
     y_pred = model.predict(X_test)
 
-
     mse = mean_squared_error(y_test, y_pred)
     mae = mean_absolute_error(y_test, y_pred)
     r_squared = r2_score(y_test, y_pred)
     rmse = root_mean_squared_error(y_test, y_pred)
-
 
     percentage_diff = np.abs((y_pred - y_test) / y_test)
     strict_accurate_predictions = percentage_diff <= threshold_percentage
